b6dc5112ccf8fe7d56d54fb755f8ff8450db14be
pitt1451 <piotr8605+z@gmail.com>
2023-05-04 21:50:08 +0200

    Get current date and add to xml file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <chrono>
+#include <ctime>
 #include "Markup.h"
 
 using namespace std;
 
+string getCurrentDate(void);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    CMarkup xml;
+
+    bool fileExists = xml.Load("users.xml");
+
+    if (!fileExists)
+    {
+        xml.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+        xml.AddElem("Users");
+    }
+
+    string CurrentDate = getCurrentDate();
+
+    xml.FindElem();
+    xml.IntoElem();
+    xml.AddElem("User");
+    xml.IntoElem();
+    xml.AddElem("UserId", "1");
+    xml.AddElem("Login", "Piotr");
+    xml.AddElem("Password", "123");
+    xml.AddElem("CurrentDate", CurrentDate);
+    xml.Save("users.xml");
+
     return 0;
 }
+
+    string getCurrentDate(void)
+    {
+        auto end = chrono::system_clock::now();
+        time_t end_time = chrono::system_clock::to_time_t(end);
+        return ctime(&end_time);
+    }
